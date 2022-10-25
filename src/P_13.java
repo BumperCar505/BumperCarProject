3a76e8ad6d8e107f3147c361e87a7b5bf211f792
MoonNight285 <65889750+MoonNight285@users.noreply.github.com>
2022-10-25 23:25:59 +0900

    코멘트관리 UI 수정

---
+++ src/P_13.java
@@ -0,0 +1,155 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URLDecoder;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+
+// CommentManage
+public class P_13 extends JFrame {
+
+	private JPanel contentPane;
+	private JTable tableCommentList;
+	private JScrollPane scCommentList;
+	private JLabel lblYellowCat;
+	private JLabel lblScore;
+	private JButton btnBackCommentMain;
+	private JButton btnHideComment;
+	private final int FONT_SIZE = 21;
+	
+	public void setFont() {
+		InputStream inputStream = null;
+		
+		// Font Setting
+		try {
+            String classPath = P_13.class.getResource("").getPath();
+            String path = URLDecoder.decode(classPath, "UTF-8");
+            inputStream = new BufferedInputStream(
+                    new FileInputStream(path + "/font/NanumBarunGothic.ttf"));
+
+            Font font = Font.createFont(Font.TRUETYPE_FONT, inputStream);
+            
+            lblScore.setFont(font.deriveFont(Font.PLAIN, FONT_SIZE));
+            lblScore.setFont(font.deriveFont(Font.PLAIN, FONT_SIZE));
+            btnHideComment.setFont(font.deriveFont(Font.BOLD, FONT_SIZE));
+            btnBackCommentMain.setFont(font.deriveFont(Font.BOLD, FONT_SIZE));
+    		// Table Font	
+            tableCommentList.setFont(font.deriveFont(Font.PLAIN, FONT_SIZE));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+        	if(inputStream != null) {
+        		try {
+            		inputStream.close();
+        		} catch(Exception e2) { 
+        			e2.printStackTrace();
+        		}
+        	}
+        }
+	}
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					P_13 frame = new P_13();
+					frame.setVisible(true);
+					frame.setFont();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public P_13() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, Size.SCREEN_W, Size.SCREEN_H);
+		this.setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null); // 이거 없으면 정상적으로 레이아웃 안그려진다..
+		
+		Object[] columns = {"Num", "고객명", "방문날짜", "서비스내용", "별점", "코멘트"};
+		Object[][] rowNames = {
+				{"1", "홍길동", "2022-02-11", "타이어교체", "★★★", "수리하는데 시간이 너무 오래걸림"},
+				{"2", "김홍도", "2022-02-13", "엔진오일교체", "★★★★★", "가성비있게 교체한 것 같습니다..."
+						+ "위치도 좋구요~^^"}
+		};
+		
+		// Text Align Center
+		DefaultTableCellRenderer render = new DefaultTableCellRenderer();
+		render.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		tableCommentList = new JTable(rowNames, columns);
+		tableCommentList.getColumn("Num").setCellRenderer(render);
+		tableCommentList.getColumn("고객명").setCellRenderer(render);
+		tableCommentList.getColumn("방문날짜").setCellRenderer(render);
+		tableCommentList.getColumn("서비스내용").setCellRenderer(render);
+		tableCommentList.getColumn("별점").setCellRenderer(render);
+		tableCommentList.getColumn("코멘트").setCellRenderer(render);
+		
+		// Column Not Move
+		tableCommentList.getTableHeader().setReorderingAllowed(false);
+		
+		// Column Change Width
+		tableCommentList.getColumn("서비스내용").setPreferredWidth(200);
+		tableCommentList.getColumn("코멘트").setPreferredWidth(500);
+		
+		// Row Change Height 
+		tableCommentList.setRowHeight(50);
+		
+		// Table Set Area
+		scCommentList = new JScrollPane(tableCommentList);
+		scCommentList.setVisible(true);
+		scCommentList.setBounds(100, 145, 1462, 750);
+		
+		contentPane.add(scCommentList);
+		
+		lblYellowCat = new JLabel("");
+		lblYellowCat.setIcon(new ImageIcon(P_13.class.getResource("/img/YellowCat.png")));
+		lblYellowCat.setBounds(710, 50, 230, 80);
+		contentPane.add(lblYellowCat);
+		
+		btnBackCommentMain = new JButton("돌아가기");
+		btnBackCommentMain.setBackground(new Color(244, 204, 204));
+		btnBackCommentMain.setBounds(690, 918, Size.BTN_B_W, Size.BTN_B_H);
+		btnBackCommentMain.setBorder(new BevelBorder(BevelBorder.RAISED, Color.red, Color.red, 
+				Color.red, Color.red));
+		contentPane.add(btnBackCommentMain);
+		
+		btnHideComment = new JButton("댓글 숨기기");
+		btnHideComment.setBackground(new Color(244, 204, 204));
+		btnHideComment.setBounds(100, 70, Size.BTN_S_W, Size.BTN_S_H);
+		btnHideComment.setBorder(new BevelBorder(BevelBorder.RAISED, Color.red, Color.red, 
+				Color.red, Color.red));
+		contentPane.add(btnHideComment);
+		
+		lblScore = new JLabel("평균 별점 : 4.0");
+		lblScore.setBounds(262, 74, 160, 42);
+		contentPane.add(lblScore);
+	}
+
+}
