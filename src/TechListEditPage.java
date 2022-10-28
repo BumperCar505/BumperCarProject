2dd0442ae744c4a93f30c6ed6515c11274bf6398
DESKTOP-6M69AE9\admin <nmk02@naver.com>
2022-10-28 17:05:04 +0900

    수정

--- src/TechListEditPage.java
+++ src/TechListEditPage.java
@@ -3,10 +3,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.border.CompoundBorder;
 
 public class TechListEditPage extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -34,6 +38,19 @@ public class TechListEditPage extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		
+		table = new JTable();
+		table.setBorder(new CompoundBorder());
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"1", "a", "010"},
+				{"2", "a", "019"},
+			},
+			new String[] {
+				"Num", "Name", "Phone"
+			}
+		));
+		contentPane.add(table);
 	}
 
 }
