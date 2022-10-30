1699a3a4512b52065e75e8a94ce791c42529d65e
YURI JUNG <dbfl14433@gmail.com>
2022-10-30 14:25:21 +0900

    추가 부분 성공

--- src/RegTec.java
+++ src/RegTec.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
@@ -23,6 +24,8 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Vector;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.JScrollBar;
 
 public class RegTec extends JFrame {
 //	여기에도 삭제, 수정 버튼이 있어야 할 것같다..
@@ -30,9 +33,14 @@ public class RegTec extends JFrame {
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField techName;
-	private JTextField tecTel;
-	private JTextField tecLv;
+	private JTextField techTel;
+	private JTextField techLv;
+	private LineBorder LineBorderRegTec1;
+//	private LineBorder LineBorderRegTec2;
 	private JTable table;
+	private JTable listTech;
+	private static int n;
+	private int flag = 0;
 
 	/**
 	 * Launch the application.
@@ -43,31 +51,6 @@ public class RegTec extends JFrame {
 				try {
 					RegTec frame = new RegTec();
 					frame.setVisible(true);
-//					String header[] = {"정비사 이름","정비사 전화번호","정비사직급"};
-//					
-//					String contents[][] = {
-//							{"백호왕","90","87"},
-//							{"왈숙이","100","99"},
-//							{"대장장이","30","25"}
-//					};
-////					
-//					DefaultTableModel model = new DefaultTableModel(contents, header);
-//					JTable table = new JTable(model);
-//					JScrollPane scrollpane = new JScrollPane(table);
-					
-//					JPanel panel = new JPanel();
-//					panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-//					
-//					
-//					
-//					JTextField nameField = new JTextField(5);
-//					JTextField sbj1 = new JTextField(3);
-//					JTextField sbj2 = new JTextField(3);
-////					JTextField sbj3 = new JTextField(3);
-//					
-//					panel.add(nameField);
-//					panel.add(sbj1);
-//					panel.add(sbj2);
 				
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -76,28 +59,37 @@ public class RegTec extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	
 	public RegTec() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, Size.SCREEN_W, Size.SCREEN_H);
+		
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		
+		
+//		좌측부분 라벨 사용해서 외곽에 선 넣어주기
+		LineBorderRegTec1 = new LineBorder(Color.black, 1, true);
+		JLabel lblNewLabel_4 = new JLabel("");
+		lblNewLabel_4.setBounds(958, 93, 545, 545);		
+		
+		
+		
+//		우측은 잠시 보류... 막혔다. 할 게 많으니까 좀 있다가 도전 하기로..
+//		LineBorderRegTec2 = new LineBorder(Color.black, 1, true);
+//		
+		
 		JPanel panel = new JPanel();
+		panel.setForeground(new Color(255, 0, 0));
 		panel.setBounds(57, 93, 535, 565);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBounds(949, 139, 684, 745);
-		
-		JButton btnTechNext = new JButton("다음");
-		btnTechNext.setBounds(696, 943, 380, 72);
-		btnTechNext.setFont(new Font("나눔바른고딕", Font.PLAIN, 21));
-		panel_1.setLayout(null);
+
+	
 		
 		textField = new JTextField();
 		textField.setBounds(218, 37, 266, 62);
@@ -110,106 +102,153 @@ public class RegTec extends JFrame {
 		panel_2.setBounds(64, 113, 564, 576);
 		panel_1.add(panel_2);
 		contentPane.setLayout(null);
-		//
-		
-		
-//		table = new JTable();
-//		table.setModel(new DefaultTableModel(
-//			new Object[][] {
-//				{"a", "a"},
-//				{"a", null},
-//			},
-//			new String[] {
-//				"정비사명", "정비사 전화번호"
-//			}
-//		));
-		table.setBounds(934, 109, 574, 537);
-		contentPane.add(table);
-//		panel.setLayout(null);
-		
-		
-		String header[] = {"정비사 이름","정비사 전화번호","정비사 직급"};
-		String contents[][] = {
-				{"김지훈","010-222-2222","대리"},
-				{"이태하","010-3333-3333","과장"}
-		};
-		DefaultTableModel model = new DefaultTableModel();
-		JTable table = new JTable(model);
-		JScrollPane scrollPane = new JScrollPane(table);
 		
 		
 		
 		
-		//
 		
-		JButton btnTechReg = new JButton("등록");
-		btnTechReg.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
-		btnTechReg.setBounds(247, 631, 182, 77);
-		panel.add(btnTechReg);
+//		입력하는 부분들
 		
 		techName = new JTextField();
+		techName.setBounds(26, 99, 206, 31);
 		techName.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
-		techName.setBounds(30, 98, 354, 60);
 		panel.add(techName);
 		techName.setColumns(10);
 		
-		tecTel = new JTextField();
-		tecTel.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
-		tecTel.setColumns(10);
-		tecTel.setBounds(30, 240, 426, 60);
-		panel.add(tecTel);
+		techLv = new JTextField();
+		techLv.setBounds(26, 347, 206, 31);
+		techLv.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
+		techLv.setColumns(10);
+		panel.add(techLv);
 		
-		tecLv = new JTextField();
-		tecLv.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
-		tecLv.setColumns(10);
-		tecLv.setBounds(30, 375, 426, 60);
-		panel.add(tecLv);
+		
+		techTel = new JTextField();
+		techTel.setBounds(26, 220, 206, 31);
+		techTel.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
+		techTel.setColumns(10);
+		panel.add(techTel);
 		contentPane.add(panel);
 		
+		
+//		라벨 모음
 		JLabel lblNewLabel = new JLabel("정비사이름");
+		lblNewLabel.setBounds(26, 45, 100, 25);
 		lblNewLabel.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
-		lblNewLabel.setBounds(30, 46, 175, 42);
 		panel.add(lblNewLabel);
 		
+		
 		JLabel lblNewLabel_1 = new JLabel("정비사 전화번호");
+		lblNewLabel_1.setBounds(26, 172, 145, 25);
 		lblNewLabel_1.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
-		lblNewLabel_1.setBounds(30, 188, 189, 42);
 		panel.add(lblNewLabel_1);
 		
+		
 		JLabel lblNewLabel_2 = new JLabel("정비사 직급");
+		lblNewLabel_2.setBounds(26, 291, 105, 25);
 		lblNewLabel_2.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
-		lblNewLabel_2.setBounds(30, 323, 175, 42);
 		panel.add(lblNewLabel_2);
 		
-		JButton btnNewButton = new JButton("등록");
-		btnNewButton.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
-		btnNewButton.addActionListener(new ActionListener() {
+		
+		
+//		우측 표 삽입
+	
+
+		String header[]= { "정비사 이름", "정비사전화번호", "정비사 직급"};
+		String contents[][] = {
+				{ "이이이", "010-111-1111", "대리"}
+			
+		};
+		DefaultTableModel model = new DefaultTableModel(contents,header);
+		JTable listTech = new JTable(model);
+		JScrollPane scrollpane = new JScrollPane(table);
+		
+
+		listTech.getColumnModel().getColumn(0).setPreferredWidth(52);
+		listTech.getColumnModel().getColumn(2).setPreferredWidth(104);
+		listTech.setBounds(994, 71, 548, 506);
+		contentPane.add(listTech);
+		JLabel lblRegTec = new JLabel("");
+		lblRegTec.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+		
+		lblRegTec.setBorder(LineBorderRegTec1);    //원하는 라벨에 사용
+		lblRegTec.setBounds(57, 93, 535, 565);
+		contentPane.add(lblRegTec);
+		
+//		DefaultTableModel tm = new DefaultTableModel(String, Object);
+	
+		
+		
+		
+//		고양이 이미지 삽입
+		JLabel lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setBounds(655, 22, 357, 146);
+		lblNewLabel_3.setIcon(new ImageIcon(RegTec.class.getResource("/img/YellowCat.png")));
+		contentPane.add(lblNewLabel_3);
+	
+		
+		
+//		등록 버튼
+		JButton btnTechReg = new JButton("등록");
+		btnTechReg.setBounds(222, 451, 73, 33);
+		btnTechReg.setFont(new Font("나눔바른고딕", Font.BOLD, 21));
+		panel.add(btnTechReg);
+		
+//		혹시나 해서 삭제 버튼(필요할 수도 있으니꺄아아)
+		JButton btnTechDel = new JButton("삭제");
+		btnTechDel.setBounds(1198, 587, Size.BTN_S_W, Size.BTN_S_H);
+		contentPane.add(btnTechDel);
+		panel.setLayout(null);
+		
+		
+//		다음버튼
+		JButton btnTechNext = new JButton("다음");
+		btnTechNext.setBounds(655, 611, 172, 49);
+		btnTechNext.setFont(new Font("나눔바른고딕", Font.BOLD, 22));
+		contentPane.add(btnTechNext);
+		
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setBounds(1542, 71, 17, 506);
+		contentPane.add(scrollBar);
+		
+	
+
+
+	
+		
+		
+//		다른 블로그에서 도전!
+//		저장 버튼 누르면 옆에 저장되게
+		btnTechReg.addActionListener(new ActionListener() {
+			
+			@Override
 			public void actionPerformed(ActionEvent e) {
+				String inputStr[] = new String[3];
+				
+				inputStr[0] = techName.getText();
+				inputStr[1] = techTel.getText();
+				inputStr[2] = techLv.getText();
+				
+				model.addRow(inputStr);
 			}
 		});
-		btnNewButton.setBounds(153, 455, 150, 51);
-		panel.add(btnNewButton);
 		
-		JLabel lblNewLabel_3 = new JLabel("");
-		lblNewLabel_3.setIcon(new ImageIcon(RegTec.class.getResource("/img/YellowCat.png")));
-		lblNewLabel_3.setBounds(655, 22, 357, 146);
-		contentPane.add(lblNewLabel_3);
 		
-		JButton btnNewButton_1 = new JButton("다음");
-		btnNewButton_1.setFont(new Font("나눔바른고딕", Font.BOLD, 22));
-		btnNewButton_1.setBounds(655, 611, 172, 49);
-		contentPane.add(btnNewButton_1);
 		
-//		Vector<String> tec = new Vector<>();
-//		tec.addElement("정비사 이름");
-//		tec.addElement("정비사 전화번호");
-//		tec.addElement("정비사 직급");
-//		
-
 		
-	
 		
-	
+//		이거 삭제하는거.
+//		btnTechDel.addActionListener(new ActionListener() {
+//			
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				if(table.getSelectedRow() == -1) {
+//					return;
+//				}
+//				else {
+//					model.removeRow(table.getSelectedRow());
+//				}
+//			}
+//		});
 
 	
 	}
